4641ae0357076828fe62d61b9e467b0269569c15
Alex Baranowski <aleksander.baranowski@yahoo.pl>
2023-07-07 17:58:14 +0200

    Frozen autocommit 2023-07-07 17:58:14.901748

--- .gitlab-ci/debian-install.sh
+++ .gitlab-ci/debian-install.sh
@@ -2,9 +2,21 @@
 
 set -ex
 
-grep -q '^VERSION=.\(9 (stretch)\|18.04.[0-9]\+ LTS\)' /etc/os-release && IS_DEBIAN_9=1 || IS_DEBIAN_9=0
+IS_DEBIAN_9=0
+IS_UBUNTU_18_04=0
+grep -q '^VERSION=.\(9 (stretch)\)' /etc/os-release && IS_DEBIAN_9=1
+grep -q '^VERSION=.\(18.04.[0-9]\+ LTS\)' /etc/os-release && IS_UBUNTU_18_04=1
 
 if [ $IS_DEBIAN_9 = 1 ]; then
+    cat > /etc/apt/sources.list <<EOF
+deb http://archive.debian.org/debian/ stretch main non-free contrib
+deb-src http://archive.debian.org/debian/ stretch main non-free contrib
+deb http://archive.debian.org/debian-security/ stretch/updates main non-free contrib
+deb-src http://archive.debian.org/debian-security/ stretch/updates main non-free contrib
+EOF
+fi
+
+if [ $IS_DEBIAN_9 = 1 -o $IS_UBUNTU_18_04 = 1 ]; then
     # pam is hosted on this release to the point chfn doesn't work.
     # It's okay on Ubuntu 16.04 and 20.04 though, so keep this version specific.
     #
--- README.md
+++ README.md
@@ -4,4 +4,4 @@ This repository contains frozen state of the repository.
 Its history is cleared. It is used only for static code
 analysis with Codacy.
 
-Commit Time: 1687864257
+Commit Time: 1688745494
